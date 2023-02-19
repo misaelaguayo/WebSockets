1f0ea132d015cd17b2f429efd718b7f1df6755e1
misael aguayo <aguayo.misael@gmail.com>
2023-02-18 18:37:28 -0600

    hot reload webpack server and sample messages

---
+++ src/game/components/messages.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+export default function Messages(): JSX.Element {
+  return (<div id="messages">
+    <p>message 1</p>
+    <p>message 1</p>
+  </div>)
+}
+
--- src/game/main.tsx
+++ src/game/main.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import Messages from './components/messages'
 
-const App = (): JSX.Element => <h1>hello</h1>;
+const App = (): JSX.Element =>
+  <div id="app">
+    <Messages />
+  </div>
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
