d87a27f2f5b196e2d6847a008f463e33e89fc015
misael aguayo <aguayo.misael@gmail.com>
2023-02-18 18:56:22 -0600

    adding css to messages

--- src/game/components/messages.tsx
+++ src/game/components/messages.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 export default function Messages(): JSX.Element {
-  return (<div id="messages">
+  return (<div className="messages">
     <p>message 1</p>
     <p>message 1</p>
   </div>)
--- src/game/main.tsx
+++ src/game/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import Messages from './components/messages'
+import './styles/App.css'
 
 const App = (): JSX.Element =>
   <div id="app">
